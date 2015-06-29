1330ab66483b0d11968fa7649c034cc2a9308b7f
juns94 <jmzarama@gmail.com>
2015-06-29 10:27:58 -0600

    Cambios Crear Carpetas
    Juan

---
+++ FacilityManagement/App_Domain/Laboratory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FacilityManagement.App_Domain
+{
+    public class Laboratory
+    {
+        private int id;
+        private string name;
+        private int capacity;
+        private string type;
+        private int facultyId;
+
+
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Capacity
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+    }
+
+
+}
