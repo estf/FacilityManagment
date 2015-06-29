46f8b6e221d1b392df2a0f5335acfc4a223abea6
Stefano Arce <estefanojose1000@hotmail.com>
2015-06-29 12:53:12 -0600

    semana 8

---
+++ FacilityManagement/App_Business/UserBusiness.cs
@@ -0,0 +1,28 @@
+﻿using FacilityManagement.App_Domain;
+using FacilityManagement.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FacilityManagement.App_Business
+{
+    public class UserBusiness
+    {
+        UserData userData;
+        public UserBusiness(){
+            userData = new UserData();
+        }
+        public void InsertReservation(User user)
+        {
+
+            userData.InsertUser(user);
+        }
+        public IEnumerable<User> SelectUser()
+        {
+
+            return userData.SelectUser();
+        }
+            
+    }
+}
---
+++ FacilityManagement/Data/UserData.cs
@@ -0,0 +1,65 @@
+﻿using FacilityManagement.App_Domain;
+using Library.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace FacilityManagement.Data
+{
+    public class UserData:BaseData
+    {
+        public UserData() { }
+        public void InsertUser(User user)
+        {
+            try
+            {
+                SqlConnection con = DatabaseConnection("open");
+                SqlCommand sqlcommand;
+                string databaseCommand = " INSERT INTO User (name,email,carne,password) values (" + "'" + user.Name + "'" + "," + "'" + user.Email + "'" + "," + "'" + user.Carne + "'" + "," + "'" + user.Pass + "'"  + ")";
+                sqlcommand = new SqlCommand(databaseCommand, con);
+                sqlcommand.ExecuteNonQuery();
+                sqlcommand.Dispose();
+                DatabaseConnection("close");
+            }
+            catch (SqlException sqlException)
+            {
+                Console.WriteLine("Database error:" + sqlException.ToString());
+
+            }
+        }
+        public IEnumerable<User> SelectUser()
+        {
+            List<User> authors = new List<User>();
+            IEnumerable<User> items = null;
+            try
+            {
+
+                SqlDataReader myReader = null;
+                SqlConnection con = DatabaseConnection("open");
+                SqlCommand sqlcommand;
+                string databaseCommand = " SELECT * FROM user";
+                sqlcommand = new SqlCommand(databaseCommand, con);
+                //Ejecutar el comando SQL
+                myReader = sqlcommand.ExecuteReader();
+                while (myReader.Read())
+                {
+                    //authors.Add(new Reservation(myReader["name"].ToString(), myReader["name"].ToString()));
+                }
+
+                DatabaseConnection("close");
+            }
+            catch (SqlException sqlException)
+            {
+                Console.WriteLine("Database error:" + sqlException.ToString());
+
+            }
+
+            items = authors;
+            return items;
+
+        }
+    }
+    
+}
