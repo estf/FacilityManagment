a565ca95243f4776a242b7d590f776629438b3c0
jhonGit <jhocalderoNN@gmail.com>
2015-06-29 12:22:15 -0600

    Class seat has been added
    create a new class Seat

---
+++ FacilityManagement/App_Domain/Seat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FacilityManagement.App_Domain
+{
+    public class Seat
+    {
+        private string SeatNumber;
+        private int LaboratoryId;
+        private int ReservationId;
+
+
+    }
+}
